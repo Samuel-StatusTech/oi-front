6006ad3255b44a049d411e7dc68c39ed020d0d90
SamuelBrunoo <samuel.k.bruno@gmail.com>
2023-08-29 10:48:28 -0300

    Group select base in Register-operators

--- src/pages/Register/Operator/index.js
+++ src/pages/Register/Operator/index.js
@@ -1,6 +1,14 @@
 import React, { useState, useEffect } from 'react';
-import { Grid, Button } from '@material-ui/core';
+import {
+  Grid,
+  Button,
+  MenuItem,
+  Select,
+  FormControl,
+  InputLabel,
+} from '@material-ui/core';
 import { useHistory } from 'react-router-dom';
+import './styles/index.css'
 
 import EaseGrid from '../../../components/EaseGrid';
 // import Drawer from "./drawer";
@@ -14,6 +22,11 @@ const Operator = () => {
   const history = useHistory();
   const [data, setData] = useState([]);
   const [id, setId] = useState(null);
+  const [type, setType] = useState('todos');
+  const [group, setGroup] = useState('todos');
+  const [search, setSearch] = useState('');
+  const [status, setStatus] = useState('todos');
+  const [groupList, setGroupList] = useState([]);
 
   const columns = [
     { title: 'Operador', field: 'name', },
@@ -59,6 +72,16 @@ const Operator = () => {
   ];
 
   useEffect(() => {
+    Api.get(`/group/getList`).then(async ({ data }) => {
+      const { success, groups } = data;
+
+      if (success) {
+        setGroupList([{ id: 'todos', name: 'Todos' }, ...groups]);
+      } else {
+        alert('Erro ao carregar os grupos');
+      }
+    });
+
     Api.get('/operator/getList').then(({ data }) => {
       console.log(data);
       setData(data.operators.sort((a, b) => {
@@ -100,6 +123,47 @@ const Operator = () => {
     }
   };
 
+  const handleQuery = async (props) => {
+    try {
+      const selectedGroup = props.group ? props.group : group;
+      const searchText = props.search !== undefined ? props.search : search;
+      const selectedStatus = props.status ? props.status : status;
+      const url = `/operator/getList?type=${props.type ? props.type : type}${selectedGroup !== 'todos' ? '&group=' + selectedGroup : ''
+        }&search=${searchText}${selectedStatus !== 'todos' ? `&status=${selectedStatus}` : ''}`;
+
+      const { data } = await Api.get(url);
+
+      if (data.success) {
+        setData(data.operators.sort((a, b) => a.status == b.status ? a.name.localeCompare(b.name) : a.status > b.status ? -1 : 1));
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  var time;
+
+  const handleType = (e) => {
+    setType(e.target.value);
+    setGroup('todos');
+    localStorage.setItem('GROUP_SAVED', 'todos');
+
+    if (time) {
+      clearTimeout(time);
+      time = null;
+    }
+
+    time = setTimeout(() => {
+      handleQuery({ type: e.target.value, group: 'todos' });
+    }, 100);
+  };
+
+  const handleGroup = (e) => {
+    setGroup(e.target.value);
+    localStorage.setItem('GROUP_SAVED', e.target.value);
+    handleQuery({ group: e.target.value });
+  };
+
   return (
     <Grid container>
       <ModalResetPassword id={id} onClose={() => setId(null)} />
@@ -108,13 +172,28 @@ const Operator = () => {
           columns={columns}
           data={data}
           toolbar={() => (
-            <ButtonRound
-              variant="contained"
-              color="primary"
-              onClick={handleGotoCreate}
-            >
-              Adicionar Operador
-            </ButtonRound>
+            <div style={{
+              display: 'flex',
+              justifyContent: 'space-between'
+            }}>
+              <ButtonRound
+                variant="contained"
+                color="primary"
+                onClick={handleGotoCreate}
+              >
+                Adicionar Operador
+              </ButtonRound>
+              <FormControl variant='outlined' size='small' fullWidth className='groupSelect'>
+                <InputLabel>Grupo</InputLabel>
+                <Select value={group} onChange={handleGroup} label='Grupo' variant='outlined' fullWidth>
+                  {groupList.map((groupItem) => (
+                    <MenuItem key={groupItem.id} value={groupItem.id}>
+                      {groupItem.name}
+                    </MenuItem>
+                  ))}
+                </Select>
+              </FormControl>
+            </div>
           )}
         />
       </Grid>
